cfc932fd18285f17a2042dbf6e407802b6b70b5b
Vishal Mewada <vishalmewada9826@gmail.com>
2023-06-30 16:35:44 +0530

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -41,9 +41,7 @@ const Login = () => {
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}>
           <Heading fontSize={"4xl"}>Sign in to your account</Heading>
-          <Text fontSize={"lg"} color={"gray.600"}>
-            to enjoy all of our cool <Link color={"blue.400"}>features</Link> ✌️
-          </Text>
+          
         </Stack>
         <Box rounded={"lg"} boxShadow={"lg"} p={8}>
           <Stack spacing={4}>
